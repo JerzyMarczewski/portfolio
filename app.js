6a07cab389334a68b440d605617a6bcfb8baf0a8
Jerzy Marczewski <jerzy.marczewski2@gmail.com>
2023-06-16 08:36:58 +0200

    added render function

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+const express = requre("express");
+const app = express();
 const ejs = require("ejs");
+const fs = require("fs");
 
-ejs.renderFile(fileName, {}, {}, callback);
+const PORT = 8000;
+
+app.get("/", (req, res) => {
+  ejs.renderFile("index.ejs", {}, {}, (err, template) => {
+    if (err) throw err;
+    else res.end(template);
+  });
+});
